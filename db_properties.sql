c096d516158b5243b622269a14d88eaa2e1b8ba4
sfilatov96 <stepa-filatov@yandex.ru>
2016-04-13 15:14:13 +0300

    база

--- db_properties.sql
+++ db_properties.sql
@@ -1,50 +1,21 @@
 DROP DATABASE IF EXISTS `forum`;
-CREATE DATABASE  IF NOT EXISTS `forum` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE  IF NOT EXISTS `forum` ;
 USE `forum`;
--- MySQL dump 10.13  Distrib 5.5.41, for debian-linux-gnu (x86_64)
---
--- Host: 127.0.0.1    Database: forum
--- ------------------------------------------------------
--- Server version	5.5.41-0ubuntu0.14.04.1
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `Forum`
---
 
 DROP TABLE IF EXISTS `Forum`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `Forum` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `short_name` varchar(255) NOT NULL,
   `user` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`),
-  UNIQUE KEY `short_name` (`short_name`),
-  KEY `user` (`user`),
-  CONSTRAINT `Forum_ibfk_1` FOREIGN KEY (`user`) REFERENCES `User` (`email`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=cp1251;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `Post`
---
 
 DROP TABLE IF EXISTS `Post`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `Post` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -64,23 +35,11 @@ CREATE TABLE `Post` (
   `childs_count` int(11) NOT NULL DEFAULT '0',
   `points` int(11) NOT NULL DEFAULT '0',
   `isRoot` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `forum` (`forum`),
-  KEY `thread` (`thread`),
-  KEY `user` (`user`),
-  CONSTRAINT `Post_ibfk_1` FOREIGN KEY (`forum`) REFERENCES `Forum` (`short_name`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `Post_ibfk_2` FOREIGN KEY (`thread`) REFERENCES `Thread` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `Post_ibfk_3` FOREIGN KEY (`user`) REFERENCES `User` (`email`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=cp1251;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `Thread`
---
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `Thread`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `Thread` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `forum` varchar(255) NOT NULL,
@@ -95,37 +54,20 @@ CREATE TABLE `Thread` (
   `dislikes` int(11) NOT NULL DEFAULT '0',
   `posts` int(11) NOT NULL DEFAULT '0',
   `points` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `forum` (`forum`),
-  KEY `user` (`user`),
-  CONSTRAINT `Thread_ibfk_1` FOREIGN KEY (`forum`) REFERENCES `Forum` (`short_name`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `Thread_ibfk_2` FOREIGN KEY (`user`) REFERENCES `User` (`email`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=cp1251;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `Thread_followers`
---
 
 DROP TABLE IF EXISTS `Thread_followers`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `Thread_followers` (
   `thread_id` int(11) NOT NULL,
   `follower_email` varchar(50) NOT NULL,
-  PRIMARY KEY (`thread_id`,`follower_email`),
-  KEY `u_id` (`follower_email`),
-  CONSTRAINT `thread_followers_ibfk_1` FOREIGN KEY (`thread_id`) REFERENCES `Thread` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  PRIMARY KEY (`thread_id`,`follower_email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `User`
---
 
 DROP TABLE IF EXISTS `User`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `User` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) DEFAULT NULL,
@@ -133,33 +75,14 @@ CREATE TABLE `User` (
   `name` varchar(255) DEFAULT NULL,
   `email` varchar(255) NOT NULL,
   `isAnonymous` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=cp1251;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `User_followers`
---
 
 DROP TABLE IF EXISTS `User_followers`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `User_followers` (
   `User` varchar(50) NOT NULL,
   `Followers` varchar(50) NOT NULL,
-  PRIMARY KEY (`User`,`Followers`),
-  KEY `u_to` (`Followers`)
+  PRIMARY KEY (`User`,`Followers`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2015-04-05 13:35:48
